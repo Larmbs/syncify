9a193dae989ed3df9550b45849a04cbd3476a0d1
Larmbs <liamisrael200@gmail.com>
2024-06-16 20:19:45 -0400

    Progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,36 @@
+use lazy_static::lazy_static;
+use std::net::{
+    TcpStream,
+    SocketAddr,
+};
+use std::sync::Mutex;
+use std::io;
 
-pub trait UDPSync {
-    
+use serde::{Deserialize, Serialize};
+
+lazy_static! {
+    static ref CONN: Mutex<Option<TcpStream>> = Mutex::new(None);
+}
+
+/// Connects to external server for syncing
+pub fn connect(addr: SocketAddr) -> io::Result<()> {
+    let mut conn = CONN.lock().unwrap();
+
+    let stream = TcpStream::connect(addr)?;
+    *conn = Some(stream);
+
+    Ok(())
+}
+
+/// Trait that allows struct to sync with other structs through server
+pub trait Sync<'a>: Serialize + Deserialize<'a> {
+    fn listen() {
+        todo!()
+    }
+    fn send() {
+        todo!()
+    }
+    fn init() {
+        todo!()
+    }
 }
