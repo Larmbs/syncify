7ca5a2b87b94692613f61915110a26b03de6008a
Larmbs <liamisrael200@gmail.com>
2024-06-16 23:06:08 -0400

    More stuff

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,30 @@ use std::net::{
     TcpStream,
     SocketAddr,
 };
-use std::sync::Mutex;
-use std::io;
+use std::sync::{
+    Mutex, 
+    PoisonError,
+};
+use std::io::{
+    self, 
+    Write,
+    Read,
+};
 
 use serde::{Deserialize, Serialize};
+use bincode::{
+    deserialize, serialize, ErrorKind
+};
 
+// Creating a shared stream object to talk with server
 lazy_static! {
     static ref CONN: Mutex<Option<TcpStream>> = Mutex::new(None);
+}
+
+/// Error type that represents errors which might occur
+pub enum SyncError {
+    SerializeError(Box<ErrorKind>),
+    MutexError()
 }
 
 /// Connects to external server for syncing
@@ -27,10 +44,27 @@ pub trait Sync<'a>: Serialize + Deserialize<'a> {
     fn listen() {
         todo!()
     }
-    fn send() {
-        todo!()
+    /// Function that sends an entire functions state
+    fn send(&mut self) {
+        let bytes = serialize(self).unwrap();
+        
+        let mut guard = CONN.lock().unwrap();
+
+        match &mut *guard {
+            Some(stream) => {
+                stream.write_all(&bytes).unwrap();
+            },
+            None => panic_no_connection(),
+        }
+
     }
     fn init() {
         todo!()
     }
+}
+
+#[inline]
+/// Functions that panics to warn of not properly initializing a connection with server prior to syncing
+fn panic_no_connection() {
+    panic!("You attempted to sync your objects without connecting to a server!");
 }
